e75e04e0a4b46a9a297d16478ab277d650b9d0fd
xzjs <xuezhijunshang@icloud.com>
2023-05-19 18:00:20 +0800

    添加结束后重置环境

--- main.py
+++ main.py
@@ -171,6 +171,7 @@ if __name__ == "__main__":
                 if msg['data'] == 'reset':
                     demo.reset()
                 elif msg['data'] == 'stop':
+                    demo.reset()
                     demo.stop = True
                 else:
                     task_list = json.loads(msg["data"])
