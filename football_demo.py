e73b392ba4fcaeaf9c2e71cbb890a0673898996c
xzjs <xuezhijunshang@icloud.com>
2023-06-01 10:34:05 +0800

    2023.6.1存档

--- football_demo.py
+++ football_demo.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-import torch.multiprocessing as mp 
+import torch.multiprocessing as mp
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime, timedelta
 import gfootball.env as football_env
 import time, pprint, json, os, importlib, shutil
 from torch.distributions import Categorical
-import torch.multiprocessing as mp 
+import torch.multiprocessing as mp
 from tensorboardX import SummaryWriter
 from train import save_args, copy_models
 from actor import *
@@ -45,8 +45,8 @@ class Demo(object):
             self.env0 = football_env.create_environment(env_name="11_vs_11_hard_stochastic_4231", representation="raw", stacked=False, logdir='/tmp/football', \
                                           write_goal_dumps=False, write_full_episode_dumps=False, render=False)
             self.env1 = football_env.create_environment(env_name="11_vs_11_hard_stochastic_4231", representation="pixels", stacked=False, logdir='/tmp/football', \
-                                        write_goal_dumps=False, write_full_episode_dumps=False, render=True) 
-        elif formation == "442": 
+                                        write_goal_dumps=False, write_full_episode_dumps=False, render=True)
+        elif formation == "442":
             self.env0.close()
             self.env1.close()
             self.env0 = football_env.create_environment(env_name="11_vs_11_hard_stochastic_442", representation="raw", stacked=False, logdir='/tmp/football', \
@@ -63,10 +63,10 @@ class Demo(object):
         self.env0 = football_env.create_environment(env_name='11_vs_11_hard_stochastic', representation="raw", stacked=False, logdir='/tmp/football', \
                                           write_goal_dumps=False, write_full_episode_dumps=False, render=False)
         self.env1 = football_env.create_environment(env_name='11_vs_11_hard_stochastic', representation="pixels", stacked=False, logdir='/tmp/football', \
-                                        write_goal_dumps=False, write_full_episode_dumps=False, render=True) 
+                                        write_goal_dumps=False, write_full_episode_dumps=False, render=True)
 
 
-    def env_step(self, obs, h_out):
+    def env_step(self, obs, h_out, fe):
         h_in = h_out
         state_dict = fe.encode(obs[0])
         state_dict_tensor = state_to_tensor(state_dict, h_in)
@@ -78,7 +78,7 @@ class Demo(object):
         obs_p, _, _, _ = self.env1.step(real_action)
         self.img_list.append(obs_p)
         return obs_p, done, obs
-    
+
     def get_start_img(self):
         obs_p, _, _, _ = self.env1.step(0)
         self.img_list.append(obs_p)
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     # "11_vs_11_kaggle" : environment used for self-play training
     # "11_vs_11_stochastic" : environment used for training against fixed opponent(rule-based AI)
     "num_processes": 50,  # should be less than the number of cpu cores in your workstation.
-    "batch_size": 32,   
+    "batch_size": 32,
     "buffer_size": 6,
     "rollout_len": 30,
 
@@ -104,12 +104,12 @@ if __name__ == "__main__":
     "grad_clip" : 3.0,
     "eps_clip" : 0.1,
 
-    "summary_game_window" : 10, 
+    "summary_game_window" : 10,
     "model_save_interval" : 300000,  # number of gradient updates bewteen saving model
 
     "trained_model_path" : None, #'/home/logs/[04-01]12.23.27/model_57018240.tar', # None use when you want to continue traning from given model.
-    "latest_ratio" : 0.5, # works only for self_play training. 
-    "latest_n_model" : 10, # works only for self_play training. 
+    "latest_ratio" : 0.5, # works only for self_play training.
+    "latest_n_model" : 10, # works only for self_play training.
     "print_mode" : False,
 
     "encoder" : "encoder_slide",
@@ -124,28 +124,28 @@ if __name__ == "__main__":
     cur_time = datetime.now() + timedelta(hours = 9)
     arg_dict["log_dir"] = "logs/" + cur_time.strftime("[%m-%d]%H.%M.%S")
     save_args(arg_dict)
-    if arg_dict["trained_model_path"] and 'kaggle' in arg_dict['env']: 
+    if arg_dict["trained_model_path"] and 'kaggle' in arg_dict['env']:
         copy_models(os.path.dirname(arg_dict['trained_model_path']), arg_dict['log_dir'])
 
     np.set_printoptions(precision=3)
     np.set_printoptions(suppress=True)
     pp = pprint.PrettyPrinter(indent=4)
     torch.set_num_threads(1)
-    
+
     fe = importlib.import_module("encoders." + arg_dict["encoder"])
     fe = fe.FeatureEncoder()
     arg_dict["feature_dims"] = fe.get_feature_dims()
     model = importlib.import_module("models." + arg_dict["model"])
     cpu_device = torch.device('cpu')
     center_model = model.Model(arg_dict)
-    
+
     if arg_dict["trained_model_path"]:
         checkpoint = torch.load(arg_dict["trained_model_path"], map_location=cpu_device)
         optimization_step = checkpoint['optimization_step']
         center_model.load_state_dict(checkpoint['model_state_dict'])
         center_model.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         arg_dict["optimization_step"] = optimization_step
-        print("Trained model", arg_dict["trained_model_path"] ,"successfully loaded") 
+        print("Trained model", arg_dict["trained_model_path"] ,"successfully loaded")
     else:
         optimization_step = 0
 
@@ -154,25 +154,27 @@ if __name__ == "__main__":
         'model_state_dict': center_model.state_dict(),
         'optimizer_state_dict': center_model.optimizer.state_dict(),
     }
-    
+
     fe_module = importlib.import_module("encoders." + arg_dict["encoder"])
     rewarder = importlib.import_module("rewarders." + arg_dict["rewarder"])
     imported_model = importlib.import_module("models." + arg_dict["model"])
-    h_out = (torch.zeros([1, 1, arg_dict["lstm_size"]], dtype=torch.float), 
+    h_out = (torch.zeros([1, 1, arg_dict["lstm_size"]], dtype=torch.float),
                 torch.zeros([1, 1, arg_dict["lstm_size"]], dtype=torch.float))
-    
+
     fe = fe_module.FeatureEncoder()
 
 
     demo = Demo(center_model)
+    img1 = demo.get_start_img()
+
     obs = demo.env0.observation()
     step = 1
     while True:
-        
+
         # TODO 推流
         # TODO 获取指令instruction
         # reset 表示重置环境 set_formation 表示设置阵容,img为返回的图片（是numpy格式）
-        instruction = 
+        instruction = 'set_formation442'
         if isinstance(instruction, str):
             if instruction == 'reset':
                 demo.env_reset()
@@ -180,20 +182,21 @@ if __name__ == "__main__":
                 obs = demo.env0.observation()
             elif "set_formation" in instruction:
                 if "442" in instruction:
+                    print(instruction)
                     demo.set_formation("442")
                     img = demo.get_start_img()
                 if "4231" in instruction:
                     demo.set_formation("4231")
                     img = demo.get_start_img()
-                if "433" in instruction:    
-                    demo.set_formation("433")   
-                    img = demo.get_start_img() 
+                if "433" in instruction:
+                    demo.set_formation("433")
+                    img = demo.get_start_img()
             else:
                 raise ValueError(f"Error instruction: {instruction}")
-        img, done, obs = demo.env_step(obs, h_out)
+        img, done, obs = demo.env_step(obs, h_out, fe)
         if done:
             break
-    
+
     print(len(demo.img_list))
     # img = demo.img_list[0]
     # im = Image.fromarray(img)
